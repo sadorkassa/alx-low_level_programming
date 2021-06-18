8b511e4f6862ba957752f646052c024b69a5c8ed
sador kassa <sadorkassa21@gmail.com>
2021-06-18 06:24:15 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
-/**
-* main - Print numbers from 00 to 99
-*
-* Return: Always 0 (Success)
-*/
-int main()
-{
-    int i, j, k;
-    int arr[3];
-    printf("Enter the first number : ");
-    scanf("%d", &arr[0]);
-    printf("Enter the second number : ");
-    scanf("%d", &arr[1]);
-    printf("Enter the third number : ");
-    scanf("%d", &arr[2]);
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            for (k = 0; k < 3; k++)
-            {
-                if (i != j && j != k && k != i)
-                {
 
-                    printf("[%d %d %d]\n", arr[i], arr[j], arr[k]);
-                }
-            }
-        }
-    }
+/**
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
