feb70761fe02643748426bec1e0531b8ba1e26a4
sador kassa <sadorkassa21@gmail.com>
2021-06-22 06:36:47 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
   * add - Adds two values
-  * @n1: The first number to be computed
-  * @n2: The second number to be computed
+  * @s1: The first number to be computed
+  * @s2: The second number to be computed
   *
   * Return: Integer value
   */
