c5f2e125907d5cbeaa2a06be0a6620ac7a702a00
sador kassa <sadorkassa21@gmail.com>
2021-06-24 04:27:18 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,27 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
- * Description: Can only use _putchar twice.
- */
+  * print_most_numbers - Print the numbers since 0 up to 9
+  *
+  * Description: Prints the numbers excluding 2 and 4
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_most_numbers(void)
 {
-	int i;
+	int x = 0;
 
-	i = 0;
-	while (i < 10)
+	for (; x <= 9; x++)
 	{
-		if (i != 2 && i != 4)
-			_putchar(i + '0');
-
-		i++;
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
+
 	_putchar('\n');
 }
