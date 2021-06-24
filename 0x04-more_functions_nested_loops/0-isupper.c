c6afb73cb2087919180d84fcd77181ec95b2e54f
sador kassa <sadorkassa21@gmail.com>
2021-06-24 02:34:01 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
   * _isupper - Check if a letter is upper
-  * @c: The number to be checked
+  * @y: The number to be checked
   *
   * Return: 1 if c is upper letter or 0 otherwise
   */
-int _isupper(int c)
+int _isupper(int y)
 {
-    if (c >= 65 && c <= 90)
+    if (y >= 65 && y <= 90)
     {
             return (1);
     }
