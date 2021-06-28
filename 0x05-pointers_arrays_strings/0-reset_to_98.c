c5a2fa46019bc2073462fa2e067d0138d4c6ff60
sador kassa <sadorkassa21@gmail.com>
2021-06-28 05:12:11 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Set pointer param to 98.
+ * @n: int type pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is **#C - Pointers, arrays and strings**
